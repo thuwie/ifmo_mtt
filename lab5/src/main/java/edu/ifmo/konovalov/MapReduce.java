72529cb01d6c83166823762e1618394b5bec4c0f
Kirill Konovalov <kirill.konovalov@sap.com>
2019-06-13 11:28:29 +0300

    Tasks optimisation

--- lab5/src/main/java/edu/ifmo/konovalov/MapReduce.java
+++ lab5/src/main/java/edu/ifmo/konovalov/MapReduce.java
@@ -1,15 +1,20 @@
 package edu.ifmo.konovalov;
 
+import lombok.extern.java.Log;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+import scala.Tuple3;
 
 
 import java.util.Objects;
+import java.util.logging.Level;
 
 import static org.apache.spark.sql.functions.*;
 
+@Log
 public class MapReduce {
     public static final String port = ":9000";
     private static String hdfsHost;
@@ -26,8 +31,11 @@ public class MapReduce {
         Dataset<Row> dataSet = sparkSession.createDataFrame(data, LogObject.class);
 
         taskOne(dataSet);
+        taskOneJavaRDD(data);
+
         taskTwo(dataSet);
         taskThree(dataSet);
+
     }
 
     /**
@@ -35,6 +43,8 @@ public class MapReduce {
      * неудачных запросов
      */
     private static void taskOne(Dataset<Row> data) {
+        //data.filter(col("httpResponseCode").between(500,599))
+
         data.filter(col("httpResponseCode").between(500, 599))
                 .groupBy("httpEndpoint")
                 .count()
@@ -79,6 +89,22 @@ public class MapReduce {
                 .coalesce(1)
                 .toJavaRDD()
                 .saveAsTextFile(hdfsHost + "/taskThree");
+    }
+
+    private static void taskOneJavaRDD(JavaRDD<LogObject> data) {
+        data.filter(logRow -> logRow.getIntResponseCode() >= 500 && logRow.getIntResponseCode() <= 599)
+                .mapToPair(logRow -> new Tuple2<>(logRow.getHttpEndpoint(), 1))
+                .reduceByKey((a, b) -> a + b)
+                .coalesce(1)
+                .saveAsTextFile(hdfsHost+"/rddOne");
+    }
+
+    private static void taskTwoJavaRDD(JavaRDD<LogObject> data) {
+        data.mapToPair(logRow -> new Tuple2<>(new Tuple3<>(logRow.getHttpTimestamp(), logRow.getHttpMethod(), logRow.getHttpTimestamp()), 1))
+                .reduceByKey((a, b) -> a + b)
+                .filter(f -> f._2() >= 10)
+                .coalesce(1)
+                .saveAsTextFile(hdfsHost+"/rddTwo");
     }
 }
 
